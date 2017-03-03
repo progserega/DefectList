44e7c7b3d7e604010c46387dabe06ae9a30bcd2a
progserega <progserega@gmail.com>
2017-03-03 14:12:45 +1000

    обработка сохранения

--- app/src/main/java/ru/drsk/progserega/defectlist/addStationBug.java
+++ app/src/main/java/ru/drsk/progserega/defectlist/addStationBug.java
@@ -1,8 +1,11 @@
 package ru.drsk.progserega.defectlist;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class addStationBug extends AppCompatActivity {
 
@@ -13,4 +16,19 @@ public class addStationBug extends AppCompatActivity {
         setContentView(R.layout.activity_add_station_bug);
         Log.d("addStationBug.onCreate()", "2");
     }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void stationSaveBug(View view) {
+        Log.d("addStationBug.stationSaveBug()", "start save data");
+
+        // TODO добавить сохранение в базу
+
+        // успешный выход из активности:
+        setResult(Activity.RESULT_OK);
+        Log.d("addStationBug.stationSaveBug()", "close activity");
+        finish();
+    }
+
 }
